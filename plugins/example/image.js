d7dfc44a648a63e21de608d98c25c4bfda526557
yusheng929 <2624367622@qq.com>
2024-09-16 13:10:24 +0800

    细节优化

--- plugins/example/image.js
+++ plugins/example/image.js
@@ -1,8 +1,8 @@
-import { resolve, join } from 'node:path'
+import { resolve } from 'node:path'
 import { readdir } from 'node:fs/promises'
 
 export class example extends plugin {
-  constructor() {
+  constructor () {
     super({
       /** 插件名称 */
       name: 'HelloWorld',
@@ -11,27 +11,47 @@ export class example extends plugin {
           /** 请求方法 */
           method: 'get',
           /** 请求路径 */
-          path: '/image/:category', // 使用 :category 作为动态路径参数
+          path: '/image/help',
           /** 执行方法 */
           fnc: 'image'
+        },
+        {
+          /** 请求方法 */
+          method: 'get',
+          /** 请求路径 */
+          path: '/image/hanxuan',
+          /** 执行方法 */
+          fnc: 'hanxuan'
         }
       ]
     })
   }
 
   /** 请求处理 */
-  async image() {
-    const category = this.req.params.category; // 从请求参数中获取动态部分
-    const imagesDir = resolve(`./public/images/images/${category}`); // 动态构建目录路径
-      const files = await readdir(imagesDir);
+  async image () {
+      const imagesDir = resolve('./public/images/images/help')
+      const files = await readdir(imagesDir)
       
       // 过滤出图片文件
-      const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file));
+      const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file))
 
       // 随机选择一张图片
-      const randomIndex = Math.floor(Math.random() * imageFiles.length);
-      const randomImage = imageFiles[randomIndex];
+      const randomIndex = Math.floor(Math.random() * imageFiles.length)
+      const randomImage = imageFiles[randomIndex]
 
-      this.res.sendFile(join(imagesDir, randomImage)); // 使用 join 函数构建文件路径
+      this.res.sendFile(resolve(imagesDir, randomImage))
+  }
+  async hanxuan () {
+      const imagesDir = resolve('./public/images/images/hanxuan')
+      const files = await readdir(imagesDir)
+      
+      // 过滤出图片文件
+      const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file))
+
+      // 随机选择一张图片
+      const randomIndex = Math.floor(Math.random() * imageFiles.length)
+      const randomImage = imageFiles[randomIndex]
+
+      this.res.sendFile(resolve(imagesDir, randomImage))
   }
 }
